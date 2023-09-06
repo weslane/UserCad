6ff2dadf0a9d325ad6e0eb429d930d1ae2518986
Weslane <weslane.almeida@gmail.com>
2023-09-06 13:36:16 -0300

    alterar use case
    - adicionar implementação

--- domain/src/main/java/com/mwsa/domain/user/usecases/PostUserUseCase.kt
+++ domain/src/main/java/com/mwsa/domain/user/usecases/PostUserUseCase.kt
@@ -1,4 +1,13 @@
 package com.mwsa.domain.user.usecases
 
-class PostUserUseCase {
+import com.mwsa.domain.user.UserRepository
+import com.mwsa.domain.user.models.User
+
+class PostUserUseCase (
+    private val repository: UserRepository,
+){
+    suspend operator fun invoke(user: User) {
+        repository.postUser(user)
+    }
+
 }
