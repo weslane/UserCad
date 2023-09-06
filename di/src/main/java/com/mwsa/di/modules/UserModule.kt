ce9e785d2b3cee99085a519b14b791e4ad9f44a4
Weslane <weslane.almeida@gmail.com>
2023-09-06 13:34:02 -0300

    adicionar module
    - adicionar user module

--- di/src/main/java/com/mwsa/di/modules/UserModule.kt
+++ di/src/main/java/com/mwsa/di/modules/UserModule.kt
@@ -1,17 +1,24 @@
 package com.mwsa.di.modules
 
+import com.mwsa.domain.user.UserRepository
+import com.mwsa.domain.user.usecases.GetUsersUseCase
+import com.mwsa.domain.user.usecases.PostUserUseCase
+import com.mwsa.infrastructure.user.UserRemoteDataSource
+import com.mwsa.infrastructure.user.data.UserRepositoryImpl
+import com.mwsa.infrastructure.user.data.remote.UserApi
+import com.mwsa.infrastructure.user.data.remote.UserRemoteDataSourceImpl
+import com.mwsa.ui.user.UserViewModel
 import org.koin.dsl.module
+import org.koin.androidx.viewmodel.dsl.viewModel
 
 val userModule = module {
-/*
-    factory { PropertyApi() }
+    factory { UserApi() }
 
-    factory<PropertyRemoteDataSource> { PropertyRemoteDataSourceImpl(get()) }
-    factory<PropertyRepository> { PropertyRepositoryImpl(get()) }
-    factory<PropertyValidator> { PropertyValidatorImpl() }
+    factory<UserRemoteDataSource> { UserRemoteDataSourceImpl(get()) }
+    factory<UserRepository> { UserRepositoryImpl(get(), get()) }
 
-    factory { GetPropertyUseCase(get()) }
-    factory { GetPropertiesUseCase(get(), get()) }
+    factory { GetUsersUseCase(get()) }
+    factory { PostUserUseCase(get()) }
 
-    viewModel { PropertiesViewModel(get()) }*/
+    viewModel { UserViewModel(get()) }
 }
